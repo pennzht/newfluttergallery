c6559dfc12c5f788e6ffa082eeb82278b1ed1472
Tianguang Zhang <penn.zht@gmail.com>
2020-04-08 16:29:31 +0200

    Comment.

--- lib/macrobenchmarks/alt_benchmarks.dart
+++ lib/macrobenchmarks/alt_benchmarks.dart
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/* This file contains benchmark tests that are used in
+   flutter_driver tests. */
+
 import 'dart:async';
 import 'dart:convert' show json;
 import 'dart:html' as html;
