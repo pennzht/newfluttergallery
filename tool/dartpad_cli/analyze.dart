8f700a0ca8a269264302d7e3358af94c051f60ee
Tianguang Zhang <penn.zht@gmail.com>
2020-08-07 16:37:47 +0200

    A working example.

--- tool/dartpad_cli/analyze.dart
+++ tool/dartpad_cli/analyze.dart
@@ -5,13 +5,13 @@ import 'package:analyzer/dart/analysis/results.dart';
 
 Future<void> main () async {
   final collection = AnalysisContextCollection(
-    includedPaths: ['/Users/tianguang/Documents/dev/gallery2/gallery/lib'],
+    includedPaths: ['/Users/tianguang/Documents/dev/gallery2/gallery/tool/dartpad_cli/analyze.dart'],
   );
 
   for (final context in collection.contexts) {
     print (context);
     final result = await context.currentSession.getResolvedUnit(
-      '/Users/tianguang/Documents/dev/gallery2/gallery/lib/demos/material/buttom_demo.dart',
+      '/Users/tianguang/Documents/dev/gallery2/gallery/tool/dartpad_cli/analyze.dart',
     );
     print (result);
     print (result.content.length);
