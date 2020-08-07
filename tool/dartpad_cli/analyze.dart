4b389db2c1411d2a3c91c1e058ec181b581a5da2
Tianguang Zhang <penn.zht@gmail.com>
2020-08-07 17:39:11 +0200

    Another successful experiment.

--- tool/dartpad_cli/analyze.dart
+++ tool/dartpad_cli/analyze.dart
@@ -1,8 +1,11 @@
 // Experimental.
 
 import 'package:analyzer/dart/analysis/analysis_context_collection.dart';
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
 
 const filePath = '/Users/tianguang/Documents/dev/gallery2/gallery/lib/demos/material/button_demo.dart';
+const indent = '  ';
 
 Future<void> main () async {
   final collection = AnalysisContextCollection(
@@ -18,6 +21,20 @@ Future<void> main () async {
     print (result.content);
     print (result.content.length);
     print (result.unit);
+
+    walkAst(result.unit);
   }
 }
 
+void walkAst(AstNode ast, [int level = 0]) {
+  print('${indent * level}|${ast.offset} -> ${ast.end}');
+  ast.accept(PrintVisitor());
+}
+
+class PrintVisitor extends GeneralizingAstVisitor<void> {
+  @override
+  void visitNode(AstNode node) {
+    print('${node.offset} -> ${node.end}');
+    node.visitChildren(PrintVisitor());
+  }
+}
