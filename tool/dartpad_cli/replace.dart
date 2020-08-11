b0c709b92979fb3a1e32b25811e823334ff1a3f6
Tianguang Zhang <penn.zht@gmail.com>
2020-08-11 15:53:42 +0200

    Add code for "replace.dart".

---
+++ tool/dartpad_cli/replace.dart
@@ -0,0 +1,41 @@
+// Experimental: Replacement.
+
+import 'package:analyzer/dart/analysis/analysis_context_collection.dart';
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
+
+const filePath = '/Users/tianguang/Documents/dev/gallery2/gallery/lib/demos/material/button_demo.dart';
+const indent = '  ';
+
+Future<void> main () async {
+  final collection = AnalysisContextCollection(
+    includedPaths: [filePath],
+  );
+
+  for (final context in collection.contexts) {
+    print (context);
+    final result = await context.currentSession.getResolvedUnit(
+      filePath,
+    );
+
+    result.unit.accept(PrintVisitor());
+  }
+}
+
+class PrintVisitor extends GeneralizingAstVisitor<void> {
+  PrintVisitor([this.level = 0]);
+
+  final int level;
+
+  @override
+  void visitNode(AstNode node) {
+    final representation = node.length <= 32 ? node.toString() : '...';
+    print(
+        '${indent * level} | '
+            '${node.offset} -> ${node.end} '
+            '$representation '
+            '<${node.runtimeType}>'
+    );
+    node.visitChildren(PrintVisitor(level + 1));
+  }
+}
