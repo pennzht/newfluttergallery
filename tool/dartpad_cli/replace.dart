09ad8a1e79501fdc04f00d85262880e821fe8988
Tianguang Zhang <penn.zht@gmail.com>
2020-08-11 16:40:38 +0200

    Add example for replacement.

--- tool/dartpad_cli/replace.dart
+++ tool/dartpad_cli/replace.dart
@@ -4,6 +4,7 @@ import 'package:analyzer/dart/analysis/analysis_context_collection.dart';
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
 import 'package:analyzer/src/dart/ast/ast.dart';
+import 'package:_fe_analyzer_shared/src/scanner/token.dart';
 
 const filePath = '/Users/tianguang/Documents/dev/gallery2/gallery/lib/demos/material/button_demo.dart';
 const indent = '  ';
@@ -27,7 +28,8 @@ class ReplacementVisitor extends GeneralizingAstVisitor<void> {
   @override
   void visitNode(AstNode node) {
     if (node is SimpleIdentifierImpl) {
-      print (node.toString());
+      print(node.token.toString());
+      node.token = StringToken(TokenType.IDENTIFIER, 'tyyyyype', 0);
     }
     node.visitChildren(ReplacementVisitor());
   }
