6ca341709095df58abd7ad1c1432d449ee787f5e
Tianguang Zhang <penn.zht@gmail.com>
2020-09-07 16:13:34 +0200

    Do more on `replace2.dart`.

--- tool/dartpad_cli/replace2.dart
+++ tool/dartpad_cli/replace2.dart
@@ -91,38 +91,13 @@ ReturnType findAncestor<AncestorType, ReturnType>(AstNode node) {
 
 class ReplacementVisitor extends GeneralizingAstVisitor<void> {
   @override
-  void visitNode(AstNode node) {
-    node.visitChildren(ReplacementVisitor());
-    // replace "type" identifiers.
-    if (node is SimpleIdentifierImpl &&
-        node.token.lexeme == 'type' &&
-        node.parent is! FieldFormalParameter &&
-        node.parent is! VariableDeclaration) {
-      AstNode replacingNode;
-      if (node.parent is PrefixedIdentifier) {
-        replacingNode = node.parent;
-      } else {
-        replacingNode = node;
-      }
-      print ('node: $replacingNode\ntoken: ${replacingNode.toString()}\nrange: ${replacingNode.offset} -> ${replacingNode.end}');
-      replacements.add(ReplacementCommand(replacingNode, '$enumName.${enumRepresentations[0]}'));
-    } else if (node is EnumConstantDeclaration && node.parent is EnumDeclaration &&
-        (node.parent as EnumDeclaration).name.name.endsWith('Type')
-    ) {
-      enumRepresentations.add(node.name.name);
-      enumName = (node.parent as EnumDeclaration).name.name;
-    } else if (node is SimpleIdentifier &&
-        node.token.lexeme == 'type' &&
-        node.parent is FieldFormalParameter) {
-      replacements.add(ReplacementCommand(node.parent, ''));
-      // print ((node.parent as FieldFormalParameter).childEntities);
-    } else if (node is SimpleIdentifier &&
-        node.token.lexeme == 'type' &&
-        node.parent is VariableDeclaration) {
-      replacements.add(ReplacementCommand(findAncestor<ClassMember, AstNode>(node), ''));
-      print ((node.parent as VariableDeclaration).childEntities);
-    } else if (node is SwitchStatement) {
-      print(node);
+  void visitSwitchStatement(SwitchStatement statement) {
+    if (isType (statement.expression)) {
+      subtreeReplace (
+        statement.parent.childEntities,
+        statement,
+        PrefixedIdentifierImpl(SimpleIdentifierImpl(SimpleToken())),
+      );
     }
   }
 }
