e64a9438f2a62d1639203fe4654cb70dbdc2220e
Tianguang Zhang <penn.zht@gmail.com>
2020-09-07 14:13:56 +0200

    Make a copy of `replace.dart`.

---
+++ tool/dartpad_cli/replace2.dart
@@ -0,0 +1,147 @@
+// Experimental: Replacement.
+
+import 'dart:io' as io;
+
+import 'package:analyzer/dart/analysis/analysis_context_collection.dart';
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:analyzer/src/dart/ast/ast.dart';
+
+//const filePath = '/Users/tianguang/Documents/dev/gallery2/gallery/lib/demos/material/button_demo.dart';
+const filePath = '/Users/tianguang/Documents/dev/gallery2/gallery/lib/demos/material/menu_demo.dart';
+const indent = '  ';
+
+class ReplacementCommand {
+  const ReplacementCommand(this.node, this.target);
+
+  final AstNode node;
+  final String target;
+}
+
+final replacements = <ReplacementCommand>[];
+final enumRepresentations = <String>[];
+String enumName;
+
+// Keep any of the following:
+// SimpleIdentifierImpl of FieldFormalParameterImpl
+// DeclaredSimpleIdentifier of VariableDeclarationImpl
+
+// Replace any of the following:
+// SimpleIdentifierImpl of PrefixedIdentifierImpl
+// SimpleIdentifierImpl of SwitchStatementImpl
+
+Future<void> main () async {
+  final collection = AnalysisContextCollection(
+    includedPaths: [filePath],
+  );
+
+  for (final context in collection.contexts) {
+    print (context);
+    final result = await context.currentSession.getResolvedUnit(
+      filePath,
+    );
+
+    result.unit.accept(ReplacementVisitor());
+    print ('=' * 80);
+
+    result.unit.accept(PrintVisitor());
+
+    print (result.unit.toSource());
+    await io.File('lib/generated/genraw.dart').writeAsString(result.unit.toString());
+    await io.File('lib/generated/gensource.dart').writeAsString(result.unit.toSource());
+  }
+
+  print(replacements);
+  print(enumRepresentations);
+
+  await handleReplacements ();
+}
+
+Future<void> handleReplacements () async {
+  int comparator(ReplacementCommand a, ReplacementCommand b) {
+    return a.node.offset - b.node.offset;
+  }
+
+  replacements.sort(comparator);
+
+  var myFile = io.File(filePath);
+  var contents = await myFile.readAsString();
+  for (var i = replacements.length - 1; i >= 0; i --) {
+    var replacement = replacements[i];
+    contents = replace(
+      contents,
+      replacement.node.offset,
+      replacement.node.end,
+      replacement.target,
+    );
+  }
+  await io.File('lib/generated/gen.dart').writeAsString(contents);
+}
+
+String replace(String original, int start, int stop, String substring)
+  => '${original.substring(0, start)}$substring${original.substring(stop)}';
+
+ReturnType findAncestor<AncestorType, ReturnType>(AstNode node) {
+  var pointer = node;
+  while (pointer != null && pointer is! AncestorType) {
+    pointer = pointer.parent;
+  }
+  return pointer as ReturnType;
+}
+
+class ReplacementVisitor extends GeneralizingAstVisitor<void> {
+  @override
+  void visitNode(AstNode node) {
+    // replace "type" identifiers.
+    if (node is SimpleIdentifierImpl &&
+        node.token.lexeme == 'type' &&
+        node.parent is! FieldFormalParameter &&
+        node.parent is! VariableDeclaration) {
+      AstNode replacingNode;
+      if (node.parent is PrefixedIdentifier) {
+        replacingNode = node.parent;
+      } else {
+        replacingNode = node;
+      }
+      print ('node: $replacingNode\ntoken: ${replacingNode.toString()}\nrange: ${replacingNode.offset} -> ${replacingNode.end}');
+      replacements.add(ReplacementCommand(replacingNode, '$enumName.${enumRepresentations[0]}'));
+    } else if (node is EnumConstantDeclaration && node.parent is EnumDeclaration &&
+        (node.parent as EnumDeclaration).name.name.endsWith('Type')
+    ) {
+      enumRepresentations.add(node.name.name);
+      enumName = (node.parent as EnumDeclaration).name.name;
+    } else if (node is SimpleIdentifier &&
+        node.token.lexeme == 'type' &&
+        node.parent is FieldFormalParameter) {
+      replacements.add(ReplacementCommand(node.parent, ''));
+      // print ((node.parent as FieldFormalParameter).childEntities);
+    } else if (node is SimpleIdentifier &&
+        node.token.lexeme == 'type' &&
+        node.parent is VariableDeclaration) {
+      replacements.add(ReplacementCommand(findAncestor<ClassMember, AstNode>(node), ''));
+      print ((node.parent as VariableDeclaration).childEntities);
+    } else if (node is SwitchStatement) {
+      print(node);
+    }
+    node.visitChildren(ReplacementVisitor());
+  }
+}
+
+class PrintVisitor extends GeneralizingAstVisitor<void> {
+  PrintVisitor([this.level = 0]);
+
+  final int level;
+
+  @override
+  void visitNode(AstNode node) {
+    final representation = node.length <= 32 ? node.toString() : '...';
+    print(
+        '${indent * level} | '
+            '${node.offset} -> ${node.end} '
+            '$representation '
+            '<${node.runtimeType}> '
+            'of: <${node.parent.runtimeType}>'
+    );
+    node.visitChildren(PrintVisitor(level + 1));
+  }
+}
