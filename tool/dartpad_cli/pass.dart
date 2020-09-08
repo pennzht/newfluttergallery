d1bbac1e128f589f4fbfef2d8ee36e386d61e1aa
Tianguang Zhang <penn.zht@gmail.com>
2020-09-08 16:35:52 +0200

    Add `LocalizationsReplacementVisitor`.

--- tool/dartpad_cli/pass.dart
+++ tool/dartpad_cli/pass.dart
@@ -163,6 +163,36 @@ class SwitchStatementReplacementVisitor extends GeneralizingAstVisitor<void> {
   }
 }
 
+class LocalizationsReplacementVisitor extends GeneralizingAstVisitor<void> {
+  // TODO: edit.
+
+  @override
+  void visitSwitchStatement(SwitchStatement statement) {
+    for (var subStatement in statement.members) {
+      if (subStatement.childEntities.toList()[1].toString() == statement.expression.toString()) {
+        print('Statements -> ${subStatement.statements}');
+
+        final effectiveCode = <String>[];
+
+        for (final statement in subStatement.statements) {
+          if (statement is BreakStatement) {
+            break;
+          }
+
+          effectiveCode.add(statement.toString());
+        }
+
+        print(effectiveCode);
+
+        replacements.add(
+          ReplacementCommand(statement, effectiveCode.join(' ')),
+        );
+      }
+    }
+  }
+}
+
+
 class PrintVisitor extends GeneralizingAstVisitor<void> {
   PrintVisitor([this.level = 0]);
 
