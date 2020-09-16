6664c1b4ca1b5a146c083c77d93cf405d6b8832d
Tianguang Zhang <penn.zht@gmail.com>
2020-09-16 15:03:09 +0200

    Add first successful recusive replacement.

--- lib/generated/gen3.dart
+++ lib/generated/gen3.dart
@@ -1,141 +1,119 @@
-// Copyright 2019 The Flutter Authors. All rights reserved.
+// Copyright 2019 The Flutter team. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:animations/animations.dart';
+import 'package:flutter/rendering.dart';
+import 'package:gallery/data/gallery_options.dart';
 
+import 'package:intl/intl.dart';
 
-// BEGIN fadeThroughTransitionDemo
+// BEGIN dataTableDemo
 
-class FadeThroughTransitionDemo extends StatefulWidget {
-  const FadeThroughTransitionDemo();
+class DataTableDemo extends StatefulWidget {
+  const DataTableDemo();
 
   @override
-  _FadeThroughTransitionDemoState createState() =>
-      _FadeThroughTransitionDemoState();
+  _DataTableDemoState createState() => _DataTableDemoState();
 }
 
-class _FadeThroughTransitionDemoState extends State<FadeThroughTransitionDemo> {
-  int _pageIndex = 0;
+class _DataTableDemoState extends State<DataTableDemo> {
+  int _rowsPerPage = PaginatedDataTable.defaultRowsPerPage;
+  int _sortColumnIndex;
+  bool _sortAscending = true;
+  _DessertDataSource _dessertsDataSource;
 
-  final _pageList = <Widget>[
-    _AlbumsPage(),
-    _PhotosPage(),
-    _SearchPage(),
-  ];
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _dessertsDataSource ??= _DessertDataSource(context);
+  }
+
+  void _sort<T>(
+    Comparable<T> Function(_Dessert d) getField,
+    int columnIndex,
+    bool ascending,
+  ) {
+    _dessertsDataSource._sort<T>(getField, ascending);
+    setState(() {
+      _sortColumnIndex = columnIndex;
+      _sortAscending = ascending;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     
-
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        title: Column(
+        title: Text('Data tables'),
+      ),
+      body: Scrollbar(
+        child: ListView(
+          padding: const EdgeInsets.all(16),
           children: [
-            Text('Fade through'),
-            Text(
-              '(${'Bottom navigation'})',
-              style: Theme.of(context)
-                  .textTheme
-                  .subtitle2
-                  .copyWith(color: Colors.white),
-            ),
-          ],
-        ),
-      ),
-      body: PageTransitionSwitcher(
-        transitionBuilder: (
-          child,
-          animation,
-          secondaryAnimation,
-        ) {
-          return FadeThroughTransition(
-            animation: animation,
-            secondaryAnimation: secondaryAnimation,
-            child: child,
-          );
-        },
-        child: _pageList[_pageIndex],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _pageIndex,
-        onTap: (selectedIndex) {
-          setState(() {
-            _pageIndex = selectedIndex;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: const Icon(Icons.photo_library),
-            // ignore: deprecated_member_use
-            title: Text('Albums'),
-          ),
-          BottomNavigationBarItem(
-            icon: const Icon(Icons.photo),
-            // ignore: deprecated_member_use
-            title: Text('Photos'),
-          ),
-          BottomNavigationBarItem(
-            icon: const Icon(Icons.search),
-            // ignore: deprecated_member_use
-            title: Text('Search'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class _ExampleCard extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    
-    final textTheme = Theme.of(context).textTheme;
-
-    return Expanded(
-      child: Card(
-        child: Stack(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  child: Container(
-                    color: Colors.black26,
-                    child: Padding(
-                      padding: const EdgeInsets.all(30),
-                      child: Ink.image(
-                        image: const AssetImage(
-                          'placeholders/placeholder_image.png',
-                          package: 'flutter_gallery_assets',
-                        ),
-                      ),
-                    ),
-                  ),
+            PaginatedDataTable(
+              header: Text('Nutrition'),
+              rowsPerPage: _rowsPerPage,
+              onRowsPerPageChanged: (value) {
+                setState(() {
+                  _rowsPerPage = value;
+                });
+              },
+              sortColumnIndex: _sortColumnIndex,
+              sortAscending: _sortAscending,
+              onSelectAll: _dessertsDataSource._selectAll,
+              columns: [
+                DataColumn(
+                  label: Text('Dessert (1 serving)'),
+                  onSort: (columnIndex, ascending) =>
+                      _sort<String>((d) => d.name, columnIndex, ascending),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        '123 photos',
-                        style: textTheme.bodyText1,
-                      ),
-                      Text(
-                        '123 photos',
-                        style: textTheme.caption,
-                      ),
-                    ],
-                  ),
+                DataColumn(
+                  label: Text('Calories'),
+                  numeric: true,
+                  onSort: (columnIndex, ascending) =>
+                      _sort<num>((d) => d.calories, columnIndex, ascending),
+                ),
+                DataColumn(
+                  label: Text('Fat (gm)'),
+                  numeric: true,
+                  onSort: (columnIndex, ascending) =>
+                      _sort<num>((d) => d.fat, columnIndex, ascending),
+                ),
+                DataColumn(
+                  label: Text('Carbs (gm)'),
+                  numeric: true,
+                  onSort: (columnIndex, ascending) =>
+                      _sort<num>((d) => d.carbs, columnIndex, ascending),
+                ),
+                DataColumn(
+                  label: Text('Protein (gm)'),
+                  numeric: true,
+                  onSort: (columnIndex, ascending) =>
+                      _sort<num>((d) => d.protein, columnIndex, ascending),
+                ),
+                DataColumn(
+                  label: Text('Sodium (mg)'),
+                  numeric: true,
+                  onSort: (columnIndex, ascending) =>
+                      _sort<num>((d) => d.sodium, columnIndex, ascending),
+                ),
+                DataColumn(
+                  label: Text('Calcium (%)'),
+                  numeric: true,
+                  onSort: (columnIndex, ascending) =>
+                      _sort<num>((d) => d.calcium, columnIndex, ascending),
+                ),
+                DataColumn(
+                  label: Text('Iron (%)'),
+                  numeric: true,
+                  onSort: (columnIndex, ascending) =>
+                      _sort<num>((d) => d.iron, columnIndex, ascending),
                 ),
               ],
-            ),
-            InkWell(
-              splashColor: Colors.black38,
-              onTap: () {},
+              source: _dessertsDataSource,
             ),
           ],
         ),
@@ -144,60 +122,393 @@ class _ExampleCard extends StatelessWidget {
   }
 }
 
-class _AlbumsPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ...List.generate(
-          3,
-          (index) => Expanded(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                _ExampleCard(),
-                _ExampleCard(),
-              ],
-            ),
-          ),
-        ),
-      ],
-    );
-  }
+class _Dessert {
+  _Dessert(this.name, this.calories, this.fat, this.carbs, this.protein,
+      this.sodium, this.calcium, this.iron);
+  final String name;
+  final int calories;
+  final double fat;
+  final int carbs;
+  final double protein;
+  final int sodium;
+  final int calcium;
+  final int iron;
+
+  bool selected = false;
 }
 
-class _PhotosPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        _ExampleCard(),
-        _ExampleCard(),
-      ],
-    );
-  }
-}
-
-class _SearchPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
+class _DessertDataSource extends DataTableSource {
+  _DessertDataSource(this.context) {
     
+    _desserts = <_Dessert>[
+      _Dessert(
+        'Frozen yogurt',
+        159,
+        6.0,
+        24,
+        4.0,
+        87,
+        14,
+        1,
+      ),
+      _Dessert(
+        'Ice cream sandwich',
+        237,
+        9.0,
+        37,
+        4.3,
+        129,
+        8,
+        1,
+      ),
+      _Dessert(
+        'Eclair',
+        262,
+        16.0,
+        24,
+        6.0,
+        337,
+        6,
+        7,
+      ),
+      _Dessert(
+        'Cupcake',
+        305,
+        3.7,
+        67,
+        4.3,
+        413,
+        3,
+        8,
+      ),
+      _Dessert(
+        'Gingerbread',
+        356,
+        16.0,
+        49,
+        3.9,
+        327,
+        7,
+        16,
+      ),
+      _Dessert(
+        'Jelly bean',
+        375,
+        0.0,
+        94,
+        0.0,
+        50,
+        0,
+        0,
+      ),
+      _Dessert(
+        'Lollipop',
+        392,
+        0.2,
+        98,
+        0.0,
+        38,
+        0,
+        2,
+      ),
+      _Dessert(
+        'Honeycomb',
+        408,
+        3.2,
+        87,
+        6.5,
+        562,
+        0,
+        45,
+      ),
+      _Dessert(
+        'Doughnut',
+        452,
+        25.0,
+        51,
+        4.9,
+        326,
+        2,
+        22,
+      ),
+      _Dessert(
+        'Apple pie',
+        518,
+        26.0,
+        65,
+        7.0,
+        54,
+        12,
+        6,
+      ),
+      _Dessert(
+        '${'Frozen yogurt'} with sugar',
+        168,
+        6.0,
+        26,
+        4.0,
+        87,
+        14,
+        1,
+      ),
+      _Dessert(
+        '${'Ice cream sandwich'} with sugar',
+        246,
+        9.0,
+        39,
+        4.3,
+        129,
+        8,
+        1,
+      ),
+      _Dessert(
+        '${'Eclair'} with sugar',
+        271,
+        16.0,
+        26,
+        6.0,
+        337,
+        6,
+        7,
+      ),
+      _Dessert(
+        '${'Cupcake'} with sugar',
+        314,
+        3.7,
+        69,
+        4.3,
+        413,
+        3,
+        8,
+      ),
+      _Dessert(
+        '${'Gingerbread'} with sugar',
+        345,
+        16.0,
+        51,
+        3.9,
+        327,
+        7,
+        16,
+      ),
+      _Dessert(
+        '${'Jelly bean'} with sugar',
+        364,
+        0.0,
+        96,
+        0.0,
+        50,
+        0,
+        0,
+      ),
+      _Dessert(
+        '${'Lollipop'} with sugar',
+        401,
+        0.2,
+        100,
+        0.0,
+        38,
+        0,
+        2,
+      ),
+      _Dessert(
+        '${'Honeycomb'} with sugar',
+        417,
+        3.2,
+        89,
+        6.5,
+        562,
+        0,
+        45,
+      ),
+      _Dessert(
+        '${'Doughnut'} with sugar',
+        461,
+        25.0,
+        53,
+        4.9,
+        326,
+        2,
+        22,
+      ),
+      _Dessert(
+        '${'Apple pie'} with sugar',
+        527,
+        26.0,
+        67,
+        7.0,
+        54,
+        12,
+        6,
+      ),
+      _Dessert(
+        '${'Frozen yogurt'} with honey',
+        223,
+        6.0,
+        36,
+        4.0,
+        87,
+        14,
+        1,
+      ),
+      _Dessert(
+        '${'Ice cream sandwich'} with honey',
+        301,
+        9.0,
+        49,
+        4.3,
+        129,
+        8,
+        1,
+      ),
+      _Dessert(
+        '${'Eclair'} with honey',
+        326,
+        16.0,
+        36,
+        6.0,
+        337,
+        6,
+        7,
+      ),
+      _Dessert(
+        '${'Cupcake'} with honey',
+        369,
+        3.7,
+        79,
+        4.3,
+        413,
+        3,
+        8,
+      ),
+      _Dessert(
+        '${'Gingerbread'} with honey',
+        420,
+        16.0,
+        61,
+        3.9,
+        327,
+        7,
+        16,
+      ),
+      _Dessert(
+        '${'Jelly bean'} with honey',
+        439,
+        0.0,
+        106,
+        0.0,
+        50,
+        0,
+        0,
+      ),
+      _Dessert(
+        '${'Lollipop'} with honey',
+        456,
+        0.2,
+        110,
+        0.0,
+        38,
+        0,
+        2,
+      ),
+      _Dessert(
+        '${'Honeycomb'} with honey',
+        472,
+        3.2,
+        99,
+        6.5,
+        562,
+        0,
+        45,
+      ),
+      _Dessert(
+        '${'Doughnut'} with honey',
+        516,
+        25.0,
+        63,
+        4.9,
+        326,
+        2,
+        22,
+      ),
+      _Dessert(
+        '${'Apple pie'} with honey',
+        582,
+        26.0,
+        77,
+        7.0,
+        54,
+        12,
+        6,
+      ),
+    ];
+  }
 
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return ListTile(
-          leading: Image.asset(
-            'placeholders/avatar_logo.png',
-            package: 'flutter_gallery_assets',
-            width: 40,
-          ),
-          title: Text('List item' + ' ${index + 1}'),
-          subtitle: Text('Secondary text'),
-        );
+  final BuildContext context;
+  List<_Dessert> _desserts;
+
+  void _sort<T>(Comparable<T> Function(_Dessert d) getField, bool ascending) {
+    _desserts.sort((a, b) {
+      final aValue = getField(a);
+      final bValue = getField(b);
+      return ascending
+          ? Comparable.compare(aValue, bValue)
+          : Comparable.compare(bValue, aValue);
+    });
+    notifyListeners();
+  }
+
+  int _selectedCount = 0;
+
+  @override
+  DataRow getRow(int index) {
+    final format = NumberFormat.decimalPercentPattern(
+      locale: GalleryOptions.of(context).locale.toString(),
+      decimalDigits: 0,
+    );
+    assert(index >= 0);
+    if (index >= _desserts.length) return null;
+    final dessert = _desserts[index];
+    return DataRow.byIndex(
+      index: index,
+      selected: dessert.selected,
+      onSelectChanged: (value) {
+        if (dessert.selected != value) {
+          _selectedCount += value ? 1 : -1;
+          assert(_selectedCount >= 0);
+          dessert.selected = value;
+          notifyListeners();
+        }
       },
-      itemCount: 10,
+      cells: [
+        DataCell(Text(dessert.name)),
+        DataCell(Text('${dessert.calories}')),
+        DataCell(Text(dessert.fat.toStringAsFixed(1))),
+        DataCell(Text('${dessert.carbs}')),
+        DataCell(Text(dessert.protein.toStringAsFixed(1))),
+        DataCell(Text('${dessert.sodium}')),
+        DataCell(Text('${format.format(dessert.calcium / 100)}')),
+        DataCell(Text('${format.format(dessert.iron / 100)}')),
+      ],
     );
+  }
+
+  @override
+  int get rowCount => _desserts.length;
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get selectedRowCount => _selectedCount;
+
+  void _selectAll(bool checked) {
+    for (final dessert in _desserts) {
+      dessert.selected = checked;
+    }
+    _selectedCount = checked ? _desserts.length : 0;
+    notifyListeners();
   }
 }
 
-// END fadeThroughTransitionDemo
+// END
